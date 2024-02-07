be01c4263b9bcd7218fac450ed90b07c9b75f554
Kartikey Srivastava <kartikeyai246@gmail.com>
2024-02-07 20:34:50 +0530

    whole app base completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:valentine/no.dart';
+import 'package:valentine/values.dart';
 import 'package:valentine/yes.dart';
 import 'package:video_player/video_player.dart';
 
@@ -18,7 +20,14 @@ class VideoApp extends StatefulWidget {
 }
 
 class _VideoAppState extends State<VideoApp> {
+  final Random _random = Random();
+
+  double _getRandomPosition(double max) {
+    return _random.nextDouble() * max;
+  }
+
   late VideoPlayerController _controller;
+  bool showMessage = false;
 
   @override
   void initState() {
@@ -37,54 +46,71 @@ class _VideoAppState extends State<VideoApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Center(
-            child: _controller.value.isInitialized
-                ? VideoPlayer(_controller)
-                : Container(),
-          ),
-          const Align(
-            widthFactor: 3,
-            heightFactor: 10,
-            child: Text(
-              'Will u be my valentine?',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Center(
+              child: _controller.value.isInitialized
+                  ? VideoPlayer(_controller)
+                  : Container(),
+            ),
+            const Align(
+              widthFactor: 3,
+              heightFactor: 10,
+              child: Text(
+                'Will you be my valentine?',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-          Align(
-            heightFactor: 12,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Yes()),
-                    );
-                  },
-                  child: const Text('Yes'),
-                ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const No()),
-                    );
-                  },
-                  child: const Text('No'),
-                ),
-              ],
+            Align(
+              heightFactor: 14,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Yes()),
+                  );
+                },
+                child: const Text('Yes'),
+              ),
             ),
-          ),
-        ],
+            if (showMessage) // Show the message only if "No" is clicked
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Center(
+                  child: Text(
+                    messages[_random.nextInt(messages.length)],
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+              ),
+            Positioned(
+              left: _getRandomPosition(screenWidth - 100),
+              top: _getRandomPosition(screenHeight - 100),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    showMessage =
+                        true; // Update the variable when "No" is clicked
+                  });
+                },
+                child: const Text('No'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
