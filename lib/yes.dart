328596d84f8ffb6466aef7580b14d6c640b762d6
Kartikey Srivastava <kartikeyai246@gmail.com>
2024-02-07 14:08:15 +0530

    both video playing

--- lib/yes.dart
+++ lib/yes.dart
@@ -1,29 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class Yes extends StatelessWidget {
+class Yes extends StatefulWidget {
   const Yes({Key? key}) : super(key: key);
 
   @override
+  State<Yes> createState() => _YesState();
+}
+
+class _YesState extends State<Yes> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _initializeController() async {
+    _controller = VideoPlayerController.asset(
+      'assets/yes.mp4',
+      videoPlayerOptions: VideoPlayerOptions(mixWithOthers: true),
+    );
+
+    await _controller.initialize();
+    _controller.setLooping(true);
+
+    // Ensure the state is updated after controller is initialized.
+    if (mounted) {
+      setState(() {});
+    }
+
+    _controller.play();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Yes Page'),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.favorite,
-              size: 100,
-              color: Colors.red,
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Happy Valentine\'s Day!',
-              style: TextStyle(fontSize: 24),
-            ),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: _controller.value.isInitialized
+              ? AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                )
+              : const CircularProgressIndicator(), // Show a loading indicator while initializing.
         ),
       ),
     );
